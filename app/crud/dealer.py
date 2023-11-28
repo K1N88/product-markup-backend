462739364dda969deb26eb8e70d91feee79a93b5
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-11-28 17:38:54 +0300

    get price by dealer.py

--- app/crud/dealer.py
+++ app/crud/dealer.py
@@ -22,19 +22,15 @@ class CRUDDealer(CRUDBase):
 
 class CRUDDealerPrice(CRUDBase):
 
-    async def create_multi(
+    async def get_product_by_dealer(
         self,
-        obj_in,
-        session: AsyncSession,
-    ) -> list[Dealer]:
-        db_objs = []
-        for item in obj_in:
-            obj_in_data = item.dict()
-            db_obj = self.model(**obj_in_data)
-            db_objs.append(db_obj)
-            session.add(db_obj)
-        await session.commit()
-        return db_objs
+        dealer: Dealer,
+        session: AsyncSession
+    ) -> Optional[List[DealerPrice]]:
+        db_objs = await session.execute(select(self.model).where(
+            self.model.dealer_id == dealer.id
+        ))
+        return db_objs.scalars().all()
 
 
 dealer_crud = CRUDDealer(Dealer)
