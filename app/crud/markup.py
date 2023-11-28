a7b9764d463a841f41503d9b03785e30bfc5b1c0
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-11-28 15:16:22 +0300

    Update markup.py

--- app/crud/markup.py
+++ app/crud/markup.py
@@ -4,5 +4,22 @@ from app.crud.base import CRUDBase
 from app.models import Markup, Statistic
 
 
-markup_crud = CRUDBase(Markup)
+class CRUDMarkup(CRUDBase):
+
+    async def create_multi(
+        self,
+        obj_in,
+        session: AsyncSession,
+    ) -> list[Markup]:
+        db_objs = []
+        for item in obj_in:
+            obj_in_data = item.dict()
+            db_obj = self.model(**obj_in_data)
+            db_objs.append(db_obj)
+            session.add(db_obj)
+        await session.commit()
+        return db_objs
+
+
+markup_crud = CRUDMarkup(Markup)
 statistic_crud = CRUDBase(Statistic)
