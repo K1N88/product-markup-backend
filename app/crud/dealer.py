627e3dc025180845f5b76f8460f988228e2289ac
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-11-28 17:15:32 +0300

    Update dealer.py

--- app/crud/dealer.py
+++ app/crud/dealer.py
@@ -20,6 +20,22 @@ class CRUDDealer(CRUDBase):
         await session.commit()
         return db_objs
 
+class CRUDDealerPrice(CRUDBase):
+
+    async def create_multi(
+        self,
+        obj_in,
+        session: AsyncSession,
+    ) -> list[Dealer]:
+        db_objs = []
+        for item in obj_in:
+            obj_in_data = item.dict()
+            db_obj = self.model(**obj_in_data)
+            db_objs.append(db_obj)
+            session.add(db_obj)
+        await session.commit()
+        return db_objs
+
 
 dealer_crud = CRUDDealer(Dealer)
 dealerprice_crud = CRUDBase(DealerPrice)
