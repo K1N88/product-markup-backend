6ae075b28a178e3829ac778ed77fa289b802823f
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-11-28 15:10:03 +0300

    Update routers.py

--- app/api/routers.py
+++ app/api/routers.py
@@ -6,6 +6,7 @@ from app.api.endpoints.dealerprice import router as dealerprice_router
 from app.api.endpoints.product import router as product_router
 from app.api.endpoints.productdealerkey import router as productdealerkey_router
 from app.api.endpoints.load_csv import router as load_csv_router
+from app.api.endpoints.markup import router as markup_router
 
 
 main_router = APIRouter()
@@ -15,3 +16,4 @@ main_router.include_router(dealer_router, prefix='/dealerprice', tags=['dealerpr
 main_router.include_router(product_router, prefix='/product', tags=['product'])
 main_router.include_router(productdealerkey_router, prefix='/productdealerkey', tags=['productdealerkey'])
 main_router.include_router(load_csv_router, prefix='/load_csv', tags=['load_csv'])
+main_router.include_router(markup_router, prefix='/', tags=['markup'])
