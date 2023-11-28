a1b24a70b5d212053683ed2a23c3847911fa6a83
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-11-28 15:16:49 +0300

    Update routers.py

--- app/api/routers.py
+++ app/api/routers.py
@@ -16,4 +16,4 @@ main_router.include_router(dealer_router, prefix='/dealerprice', tags=['dealerpr
 main_router.include_router(product_router, prefix='/product', tags=['product'])
 main_router.include_router(productdealerkey_router, prefix='/productdealerkey', tags=['productdealerkey'])
 main_router.include_router(load_csv_router, prefix='/load_csv', tags=['load_csv'])
-main_router.include_router(markup_router, prefix='/', tags=['markup'])
+main_router.include_router(markup_router, prefix='/markup', tags=['markup'])
