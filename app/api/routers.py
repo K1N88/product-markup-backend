32fc38eafbd2d13a1e5ab17a472627817245ce71
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-11-28 14:42:24 +0300

    Update routers.py

--- app/api/routers.py
+++ app/api/routers.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.endpoints.user import router as user_router
 from app.api.endpoints.dealer import router as dealer_router
 from app.api.endpoints.product import router as product_router
+from app.api.endpoints.load_csv import router as load_csv_router
 
 
 main_router = APIRouter()
@@ -10,3 +11,5 @@ main_router.include_router(user_router, tags=['users'])
 main_router.include_router(dealer_router, prefix='/dealer', tags=['dealers'])
 main_router.include_router(product_router, prefix='/product',
                            tags=['products'])
+main_router.include_router(load_csv_router, prefix='/load_csv',
+                           tags=['load_csv'])
